8b9808a9ad06ca65532fe6f42f4b13dd2e9582e9
Alena <alenapumprova@seznam.cz>
2025-02-24 23:44:12 +0100

    adding bubble effect

--- script.js
+++ script.js
@@ -52,6 +52,152 @@ window.addEventListener('resize', updateIndicator);
 updateIndicator();
 
 
+/*** Bubbles */
+(function bubblesCursor() {
+
+  let lastTime = 0;
+function onMouseMove(e) {
+    const now = Date.now();
+    if (now - lastTime > 700) { // Generuje bublinu maximálně každých 100 ms
+        cursor.x = e.clientX;
+        cursor.y = e.clientY;
+        addParticle(cursor.x, cursor.y);
+        lastTime = now;
+    }
+}
+  
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var cursor = {x: width/2, y: width/2};
+  var particles = [];
+  
+  function init() {
+    bindEvents();
+    loop();
+  }
+  
+  // Bind events that are needed
+  function bindEvents() {
+    document.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('resize', onWindowResize);
+  }
+  
+  function onWindowResize(e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+  }
+  
+  function onTouchMove(e) {
+    if( e.touches.length > 0 ) {
+      for( var i = 0; i < e.touches.length; i++ ) {
+        addParticle(e.touches[i].clientX, e.touches[i].clientY);
+      }
+    }
+  }
+  
+  function onMouseMove(e) {    
+    cursor.x = e.clientX;
+    cursor.y = e.clientY;
+    
+    addParticle( cursor.x, cursor.y);
+  }
+  
+  function addParticle(x, y) {
+    var particle = new Particle();
+    particle.init(x, y);
+    particles.push(particle);
+  }
+  
+  function updateParticles() {
+    
+    // Update
+    for( var i = 0; i < particles.length; i++ ) {
+      particles[i].update();
+    }
+    
+    // Remove dead particles
+    for( var i = particles.length - 1; i >= 0; i-- ) {
+      if( particles[i].lifeSpan < 0 ) {
+        particles[i].die();
+        particles.splice(i, 1);
+      }
+    }
+    
+  }
+  
+  function loop() {
+    requestAnimationFrame(loop);
+    updateParticles();
+  }
+  
+  /**
+   * Particles
+   */
+  
+  function Particle() {
+
+    this.lifeSpan = 600; //ms
+    this.initialStyles ={
+      "position": "absolute",
+      "display": "block",
+      // "pointerEvents": "none",
+      "z-index": "10000000",
+      "width": "15px",
+      "height": "15px",
+      "will-change": "transform",
+      "background": "#e6f1f710",
+      "box-shadow": "-1px 0px 3px rgba(160, 220, 255, 0.20), 0px -1px 3px rgba(107, 173, 211, 0.20), 1px 0px 1px rgba(58, 146, 197, 0.20), 0px 1px rgba(58, 146, 197, 0.57)",
+       "border-radius": "50%",
+      "overflow": "hidden"
+    };
+
+    // Init, and set properties
+    this.init = function(x, y) {
+
+      this.velocity = {
+        x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 20),
+        y: (-.4 + (Math.random() * -1))
+      };
+      
+      this.position = {x: x - 10, y: y - 10};
+
+      this.element = document.createElement('span');
+      applyProperties(this.element, this.initialStyles);
+      this.update();
+      
+      document.body.appendChild(this.element);
+    };
+    
+    this.update = function() {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      
+      // Update velocities
+      this.velocity.x += (Math.random() < 0.5 ? -1 : 1) * 1 / 100;
+      this.velocity.y -= Math.random() / 1000;
+      this.lifeSpan--;
+      
+      this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + ( 0.2 + (250 - this.lifeSpan) / 250) + ")";
+    }
+    
+    this.die = function() {
+      this.element.parentNode.removeChild(this.element);
+    }
+  }
+  
+  /**
+   * Utils
+   */
+  
+  // Applies css `properties` to an element.
+  function applyProperties( target, properties ) {
+    for( var key in properties ) {
+      target.style[ key ] = properties[ key ];
+    }
+  }
+  
+  init();
+})();
 
 // /*Scrolování k formuláři*/
 // $(document).ready(function () {
